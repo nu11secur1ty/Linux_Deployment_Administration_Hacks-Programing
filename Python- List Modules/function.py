5897bb79359cbdafc9cd73947f89fdb01ddc6e6c
nu11secur1ty <systadmi@abv.bg>
2018-04-24 07:36:20 -0700

    Update function.py

--- Python- List Modules/function.py
+++ Python- List Modules/function.py
@@ -7,7 +7,3 @@ import re
 
 # print all names exported by the module
 print dir(re)
-
-# sample output:
-
-# ['DEBUG', 'DOTALL', 'I', 'IGNORECASE', 'L', 'LOCALE', 'M', 'MULTILINE', 'S', 'Scanner', 'T', 'TEMPLATE', 'U', 'UNICODE', 'VERBOSE', 'X', '_MAXCACHE', '__all__', '__builtins__', '__doc__', '__file__', '__name__', '__package__', '__version__', '_alphanum', '_cache', '_cache_repl', '_compile', '_compile_repl', '_expand', '_locale', '_pattern_type', '_pickle', '_subx', 'compile', 'copy_reg', 'error', 'escape', 'findall', 'finditer', 'match', 'purge', 'search', 'split', 'sre_compile', 'sre_parse', 'sub', 'subn', 'sys', 'template']
