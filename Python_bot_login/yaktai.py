0d001ad720c84a38e9abd87361964a11e9232782
nu11secur1ty <systadmi@abv.bg>
2018-05-19 15:37:10 +0300

    Update yaktai.py

--- Python_bot_login/yaktai.py
+++ Python_bot_login/yaktai.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 
+
 def loginBOT(usr, pas):
-        br = webdriver.Firefox()
-        br.get("https://www.example.com/")
+	br = webdriver.Firefox()
+	br.get("https://web.facebook.com/")
 
-        user = br.find_element_by_css_selector("#email")
-        user.clear()
-        user.send_keys(usr)
+	user = br.find_element_by_css_selector("#email")
+	user.clear()
+	user.send_keys(usr)
 
-        passwd = br.find_element_by_css_selector("#pass")
-        passwd.clear()
-        passwd.send_keys(pas)
+	passwd = br.find_element_by_css_selector("#pass")
+	passwd.clear()
+	passwd.send_keys(pas)
 
-        btn = br.find_element_by_css_selector("#u_0_2")
-        btn.click()
+	btn = br.find_element_by_css_selector("#u_0_5")
+	btn.click()
+
 
 if __name__ == "__main__":
-
-        loginBOT('user or email', 'password')
+	loginBOT('username or email', 'yourpassword')
