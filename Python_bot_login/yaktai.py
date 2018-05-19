151040c10f2b2904ea51df1072428957b1e68ff4
nu11secur1ty <systadmi@abv.bg>
2018-05-19 11:58:38 +0300

    Create yaktai.py

---
+++ Python_bot_login/yaktai.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+
+def loginBOT(usr, pas):
+	br = webdriver.Firefox()
+	br.get("https://web.facebook.com/")
+
+	user = br.find_element_by_css_selector("#email")
+	user.clear()
+	user.send_keys(usr)
+
+	passwd = br.find_element_by_css_selector("#pass")
+	passwd.clear()
+	passwd.send_keys(pas)
+
+	btn = br.find_element_by_css_selector("#u_0_5")
+	btn.click()
+
+
+if __name__ == "__main__":
+loginBOT('username or email', 'yourpassword')
