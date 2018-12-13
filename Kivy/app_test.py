c4f9e78d2bf6a22865e5724a2a86ac098da3d835
nu11secur1ty <venvaropt@gmail.com>
2018-12-13 17:22:24 +0200

    Update app_test.py

--- Kivy/app_test.py
+++ Kivy/app_test.py
@@ -1,14 +1,11 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.10.1') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
-
 
 class TaratorApp(App):
 
     def build(self):
         return Label(text='Some text here :)')
-
-
 if __name__ == '__main__':
     TaratorApp().run()
