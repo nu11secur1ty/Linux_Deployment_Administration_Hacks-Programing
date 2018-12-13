ff8484230686cc3a71fe7772869cbd735831146c
nu11secur1ty <venvaropt@gmail.com>
2018-12-13 16:05:19 +0200

    Create app_test.py

---
+++ Kivy/app_test.py
@@ -0,0 +1,16 @@
+```python
+import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class TaratorApp(App):
+
+    def build(self):
+        return Label(text='Some text here :)')
+
+
+if __name__ == '__main__':
+    TaratorApp().run()
+```
