7753a1b6ec8fa215ea8ed42b0306ed608bda7e53
nu11secur1ty <venvaropt@gmail.com>
2018-12-19 15:34:58 +0200

    Update browser.py

--- Kivy/Button to open browser/browser.py
+++ Kivy/Button to open browser/browser.py
@@ -8,18 +8,15 @@ import webbrowser
 Builder.load_string('''
 <MenuScreen>:
     GridLayout:
-        padding: 5
-        spacing: 5
         cols: 1
-        padding: root.width*0.1
+        padding: root.width*0.3
         Button:
             background_normal: ''
-            background_color:(0, 1, 0, .5)
-            text: 'PLAY'
+            background_color:(0.862, 0.079, 0.235, 0.9)
+            text: 'ENTER'
             font_size: '20sp'
             on_press: root.val1()
 ''')
-
 class MenuScreen(Screen):
     def val1(self):
         webbrowser.open("http://google.com/")
