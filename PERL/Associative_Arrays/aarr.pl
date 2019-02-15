13082134961d4dab57581b57c275e59838b168f7
nu11secur1ty <venvaropt@gmail.com>
2019-02-15 16:19:07 +0200

    Update aarr.pl

--- PERL/Associative_Arrays/aarr.pl
+++ PERL/Associative_Arrays/aarr.pl
@@ -1,1 +1,19 @@
-asdd
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+my(%weekday, $key, $value);
+   # The each function retrieves both keys and values from a hash
+   %weekday=(
+      'Mon' => 'Monday',
+      'Tue' => 'Tuesday',
+      'Wed' => 'Wednesday',
+      'Thu' => 'Thursday',
+      'Fri' => 'Friday',
+      'Sat' => 'Saturday',
+      'Sun' => 'Sunday',
+   );
+        while(($key,$value)=each(%weekday)){
+        print "$key = $value\n";
+   }
