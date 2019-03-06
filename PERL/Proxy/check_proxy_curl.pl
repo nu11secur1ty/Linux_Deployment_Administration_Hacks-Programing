40dbad1c9243d7590375b9d5b1c5ec0298a0bd89
nu11secur1ty <venvaropt@gmail.com>
2019-03-06 17:44:41 +0200

    Update check_proxy_curl.pl

--- PERL/Proxy/check_proxy_curl.pl
+++ PERL/Proxy/check_proxy_curl.pl
@@ -10,7 +10,7 @@ our @proxys = ('0.0.0.0 0000', '0.0.0.0 0000', '0.0.0.0 0000',
 
 	foreach (@proxys) {
 		print "Proxy testing $_\n";
-        		our $check_proxy = `nc -vz $_ >> /path/outputproxy.txt 2>&1`;
+        		our $check_proxy = `curl -x $_  http://www.example.com/ >> /path/outputproxy.txt 2>&1`;
 		print "proxies is tested\n---------------------\n";
 	}
 	     my $clean_nc = `pgrep -f nc`;
