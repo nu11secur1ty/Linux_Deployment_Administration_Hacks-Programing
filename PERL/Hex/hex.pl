bca0fcf050e7ffc1ba8f96960dc997980fb412f9
nu11secur1ty <venvaropt@gmail.com>
2019-03-12 18:46:14 +0200

    Update hex.pl

--- PERL/Hex/hex.pl
+++ PERL/Hex/hex.pl
@@ -6,5 +6,5 @@ use diagnostics;
 
 my $string = "596f757220636f7079206f66207468697320636f64652066726f6d20736f6d657768657265";
  	$string =~ s/([a-fA-F0-9][a-fA-F0-9])/chr(hex($1))/eg;
-print "$string\n";
+  print "$string\n";
 exit 0;
