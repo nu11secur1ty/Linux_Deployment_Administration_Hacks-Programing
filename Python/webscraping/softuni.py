49fe8c7c5496b120b4e2a470595346d4371f1e11
nu11secur1ty <venvaropt@gmail.com>
2019-03-31 22:55:14 +0300

    Create softuni.py

---
+++ Python/webscraping/softuni.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
+
+# url investigated
+urlpage = 'https://softuni.bg/blog/interview-ines-ivanova-python-development-module?fbclid=IwAR1d-panjpa9QMMNEfLG5VKxqcXtQoQkMxv0bfPwHsdw3OJBNBcJffbHucc'
+page = urllib.request.urlopen(urlpage)
+soup = BeautifulSoup(page, 'html.parser')
+er = soup.find('div', attrs={'class': 'col-md-8 col-sm-12 col-xs-12 news-content detail'})
+er1 = soup.find_all('h3')
+print("\n")
+print("Please check all bg text\n\n\n", er)
+print("\n")
+print("Specially\n\n\n", er1)
