d67cf5150a91ef82698959ec1a6edbbf5d24472c
nu11secur1ty <venvaropt@gmail.com>
2019-04-10 21:36:05 +0300

    Update hex.pl

--- PERL/Hex/hex.pl
+++ PERL/Hex/hex.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $kurec = "596f757220636f7079206f66207468697320636f64652066726f6d20736f6d657768657265";
+my $kurec = "68747470733a2f2f7777772e6e75313173656375723174792e636f6d2f";
 	$kurec =~ s/([a-fA-F0-9][a-fA-F0-9])/chr(hex($1))/eg;
 	print "$kurec\n";
 exit 0;
