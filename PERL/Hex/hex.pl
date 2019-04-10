2f18d49aae4b55a8fb74c3a6c119b224a4e1833d
nu11secur1ty <venvaropt@gmail.com>
2019-04-10 21:37:20 +0300

    Update hex.pl

--- PERL/Hex/hex.pl
+++ PERL/Hex/hex.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $kurec = "68747470733a2f2f7777772e6e75313173656375723174792e636f6d2f";
-	$kurec =~ s/([a-fA-F0-9][a-fA-F0-9])/chr(hex($1))/eg;
-	print "$kurec\n";
+my $kRtrdfs = "68747470733a2f2f7777772e6e75313173656375723174792e636f6d2f";
+	$kRtrdfs =~ s/([a-fA-F0-9][a-fA-F0-9])/chr(hex($1))/eg;
+	print "$kRtrdfs\n";
 exit 0;
