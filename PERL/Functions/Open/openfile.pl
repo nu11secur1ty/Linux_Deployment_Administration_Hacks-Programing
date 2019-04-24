1848e97da8727a3de2975eed93dd452cc7575cb3
nu11secur1ty <venvaropt@gmail.com>
2019-04-24 10:26:13 +0300

    Update openfile.pl

--- PERL/Functions/Open/openfile.pl
+++ PERL/Functions/Open/openfile.pl
@@ -1,3 +1,9 @@
 #!/usr/bin/perl
-open(my $fh, "<", "input.txt")
-  or die "Can't open < input.txt: $!";
+# author @nu11secur1ty
+use strict;
+use warnings;
+use diagnostics;
+
+open(my $fh, "+>> output.txt")
+or die "Can't open > output.txt: $!";
+
