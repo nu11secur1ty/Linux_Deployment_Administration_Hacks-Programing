abf526d0f77dd49a8913ffb696e28b6e1a74e26a
nu11secur1ty <venvaropt@gmail.com>
2019-04-25 10:25:50 +0300

    Create flock.pl

---
+++ PERL/Flock/flock.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+print "Type youtr messagae here...\n";
+my $msg = <STDIN>;
+# import LOCK_* and SEEK_END constants
+use Fcntl qw(:flock SEEK_END);
+sub lock {
+    my ($fh) = @_;
+    flock($fh, LOCK_EX) or die "Cannot lock mailbox - $!\n";
+    # and, in case someone appended while we were waiting...
+    seek($fh, 0, SEEK_END) or die "Cannot seek - $!\n";
+}
+sub unlock {
+    my ($fh) = @_;
+    flock($fh, LOCK_UN) or die "Cannot unlock mailbox - $!\n";
+}
+open(my $mbox, "+>>", "/home/domains/WORK/Perl/flock/mamuli.$ENV{'USER'}")
+
+    or die "Can't open mailbox: $!";
+lock($mbox);
+print $mbox $msg;
+unlock($mbox);
