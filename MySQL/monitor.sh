09b54550d3403ad0de58dd372607d0334bb3627f
nu11secur1ty <venvaropt@gmail.com>
2019-05-09 15:35:16 +0300

    Create monitor.sh

---
+++ MySQL/monitor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# Author V.Varbanovski @nu11secur1ty
+
+# Input your user
+user=""
+# Input your password
+password=""
+
+# Checking a global status
+	mysql -u $user -p$password -e "SHOW GLOBAL STATUS;"
+		echo "Processlist";
+	mysql -u $user -p$password -e "SHOW PROCESSLIST;"
+		echo "Queriesi";
+	mysql -u $user -p$password -e "SHOW FULL PROCESSLIST\G;"
+		echo "WARNINGS";
+	mysql -u $user -p$password -e "SHOW COUNT(*) WARNINGS;"
+exit 0;
