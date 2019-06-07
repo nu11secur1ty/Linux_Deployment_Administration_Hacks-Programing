9323aa844e441cd1c2f37961fd2a62ffbd776632
nu11secur1ty <venvaropt@gmail.com>
2019-06-07 09:50:45 +0300

    Create lili.sh

---
+++ List_files_above_x_days_old/lili.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# author V.Varbanovski 
+
+# declarete all folders in array
+# folder0 folder1 folder2
+
+tupotii=( folder1 folder2 folder3 )
+
+for i in "${tupotii[@]}"
+do       
+	find $i -mtime +5 | xargs ls -ltr;
+	#find $i -type f -mmin +1 -exec rm -fv {} \;
+done
+	echo "Djanki"
+exit 0;
