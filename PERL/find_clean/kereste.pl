2e17711f745cf3e4c450bfd287e5a94de062f6b2
nu11secur1ty <venvaropt@gmail.com>
2020-07-19 10:00:37 +0300

    Create kereste.pl

---
+++ PERL/find_clean/kereste.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# author V.Varbanovski @nu11secur1ty
+use strict;
+use warnings;
+use diagnostics;
+# All folders
+# Cleaning all folders in array
+our @folders = ('folder/*', 'folder2/*', 'folder3/*', 'folder4/*', 'folder5/*',
+             'folder6/*', 'folder7/*', 'folder8/*', 'folder9/*',
+             'folder10/*', 'folder11/*', 'folder12/*', 'folder13/*',
+             'folder14/*', 'folder15/*', 'folder16/*', 'folder17/*', 'folder18/*',
+             'folder19/*', 'folder20/*', 'folder20/*', 'folder21/*',
+             'folder22/*', 'folder23/*');
+
+foreach (@folders) {
+        our $check_bots_clean = `rm -rf $_`;
+}
+        print "Cleaning is finished\n";
+exit 0;
