cc5a0f7976251927f9a6675152a16a8a6f72bd6c
nu11secur1ty <venvaropt@gmail.com>
2020-07-19 10:34:36 +0300

    Create kogiagotrie.sh

---
+++ PERL/find_clean/kogiagotrie.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# author V.Varbanovski 
+# Module for cleanpl
+# declarete all folders in array
+
+# debug
+#touch stupid.txt
+
+folders=( folder folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10 folder11 folder12
+        folder13 folder14 folder15 folder16 folder17 folder18 folder19 folder20 folder21 folder22 folder23 )
+
+# This a test array elements
+# test=( tex pex )
+
+for i in "${folders[@]}"
+do
+        find $i -type f -mtime +7 -exec rm -fv {} \;
+        #find $i -type f -mmin +3 -exec rm -fv {} \;
+done
+        echo "Musmule"
+exit 0;
