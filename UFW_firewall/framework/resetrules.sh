e8748ae430dba6a379dd66aa7fcc8025a9f9f8fa
nu11secur1ty <venvaropt@gmail.com>
2024-01-12 11:23:44 +0200

    Update resetrules.sh

--- UFW_firewall/framework/resetrules.sh
+++ UFW_firewall/framework/resetrules.sh
@@ -4,12 +4,14 @@ ufw --force reset
 cd /etc/ufw/
 rm -rf *.rules.*
 sleep 5;
-#ufw allow 22/tcp
-#ufw allow 80/tcp
-#ufw allow 443/tcp
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
 ufw --force enable
 ufw reload
 cd /opt/firewall
 bash iptablesreset.sh
+ufw --force enable
+ufw --force disable
 ufw --force enable
 ufw status
